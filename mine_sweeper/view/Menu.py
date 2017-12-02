12a764420a8087de1765413771eaaec94ccfb429
Belal <billy.fcih@yahoo.com>
2017-12-02 16:09:50 +0200

    Add background to the menu

--- mine_sweeper/view/Menu.py
+++ mine_sweeper/view/Menu.py
@@ -10,43 +10,44 @@ from mine_sweeper.controller.MouseController import *
 class Menu:
     def __init__(self, master):
         self.master = master
+        self.master.title("Minesweeper")
         self.frame = Frame(self.master)
 
-        '''root.geometry('640x640')
-            #resizing background to be responsive
-            def resize_image(event):
-                new_width = event.width
-                new_height = event.height
-                image = copy_of_image.resize((new_width, new_height))
-                photo = ImageTk.PhotoImage(image)
-                label.config(image=photo)
-                label.image = photo  # avoid garbage collection
+        self.image = Image.open('resources/13950123000568_PhotoI.jpg')
+        self.img_copy = self.image.copy()
 
+        self.background_image = ImageTk.PhotoImage(self.image)
 
-            image = Image.open('resources/13950123000568_PhotoI.jpg')
-            copy_of_image = image.copy()
-            photo = ImageTk.PhotoImage(image)
-            label = ttk.Label(root, image=photo)
-            label.bind('<Configure>', resize_image)
-            label.pack(fill=BOTH, expand=YES)
-            Board(root,(8, 8),game_board)'''
+        self.background = Label(self.frame, image=self.background_image)
+        self.background.pack(fill=BOTH, expand=YES)
+        self.background.bind('<Configure>', self._resize_image)
 
-        self.widthLbl = Label(self.frame, text='Width: ')
+        self.widthLbl = Label(self.background, text='Width: ')
         self.widthLbl.pack()
-        self.widthEntry = Entry(self.frame)
+        self.widthEntry = Entry(self.background)
         self.widthEntry.pack()
 
-        self.heightLbl = Label(self.frame, text='Height: ')
+        self.heightLbl = Label(self.background, text='Height: ')
         self.heightLbl.pack()
-        self.heightEntry = Entry(self.frame)
+        self.heightEntry = Entry(self.background)
         self.heightEntry.pack()
 
-        self.AIBtn = Button(self.frame, text='AI Controller', command=lambda: self.invokeBoard(AiController))
+        self.AIBtn = Button(self.background, text='AI Controller', command=lambda: self.invokeBoard(AiController))
         self.AIBtn.pack()
-        self.MouseBtn = Button(self.frame, text='Mouse Controller', command=lambda: self.invokeBoard(MouseController))
+        self.MouseBtn = Button(self.background, text='Mouse Controller', command=lambda: self.invokeBoard(MouseController))
         self.MouseBtn.pack()
 
-        self.frame.pack()
+        self.frame.pack(fill=BOTH, expand=YES)
+
+    def _resize_image(self, event):
+
+        new_width = event.width
+        new_height = event.height
+
+        self.image = self.img_copy.resize((new_width, new_height))
+
+        self.background_image = ImageTk.PhotoImage(self.image)
+        self.background.configure(image=self.background_image)
 
     def invokeBoard(self, controller):
         try:
--- mine_sweeper/view/main.py
+++ mine_sweeper/view/main.py
@@ -5,24 +5,5 @@ if __name__ == "__main__":
 
     root = Tk()
     root.minsize(640, 640)
-    '''root.geometry('640x640')
-    #resizing background to be responsive
-    def resize_image(event):
-        new_width = event.width
-        new_height = event.height
-        image = copy_of_image.resize((new_width, new_height))
-        photo = ImageTk.PhotoImage(image)
-        label.config(image=photo)
-        label.image = photo  # avoid garbage collection
-
-
-    image = Image.open('resources/13950123000568_PhotoI.jpg')
-    copy_of_image = image.copy()
-    photo = ImageTk.PhotoImage(image)
-    label = ttk.Label(root, image=photo)
-    label.bind('<Configure>', resize_image)
-    label.pack(fill=BOTH, expand=YES)'''
-
     Menu(root)
     root.mainloop()
-
