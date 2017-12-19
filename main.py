e03b6c45365aacc1b3febe0e8664c4cd3b523412
mostafa7azem <mostafahazem144@yahoo.com>
2017-12-20 00:15:54 +0200

    added console version  under upgrade for now

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from tkinter import *
 from mine_sweeper.view.menu import Menu
+from mine_sweeper.controller.game_board import GameBoard
+from mine_sweeper.controller.ai_controller import AiController
+from mine_sweeper.model.node import Node
 
 '''
     list of params:
@@ -16,6 +19,15 @@ if __name__ == "__main__":
     # use ai_controller.wait_till_ai_finish()
     # to prevent it from closing
     # hf
+    print("welcome\n")
+    print("choose width / height\n")
+    width=int(input("enter width:\n"))
+    height=int(input("enter height:\n"))
+    game_board = GameBoard.get_instance()
+    game_board.generate_initial_state(height, width)
+    controller = AiController(game_board ,GameBoard.get_instance().discover)
+    controller.start_ai_solver()
+    controller.wait_till_ai_finish()
     root = Tk()
     root.minsize(640, 640)
     Menu(root)
