9ba1210c5efb7b9e793c1f1ade4b4706e970d5a5
kareem <karim.salah2048@gmail.com>
2017-12-19 17:48:05 +0200

    add todos and docs for the air controller

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from tkinter import *
 from mine_sweeper.view.menu import Menu
 
-if __name__ == "__main__":
+'''
+    list of params:
+    -c              : start in console mode (NO UI IS INITIALIZED) 
+    -s (size)       :control the size of the board
+'''
 
+if __name__ == "__main__":
+    # TODO
+    # based on args
+    # initialize GameBoard
+    # initialize Ai_controller
+    # the console version will close immediately before finishing
+    # use ai_controller.wait_till_ai_finish()
+    # to prevent it from closing
+    # hf
     root = Tk()
     root.minsize(640, 640)
     Menu(root)
--- mine_sweeper/controller/ai_controller.py
+++ mine_sweeper/controller/ai_controller.py
@@ -3,26 +3,67 @@ from threading import *
 from mine_sweeper.controller.game_board import GameBoard
 from mine_sweeper.model.node import Node
 
+"""
+    in this docs iam describing the role of each function 
+    its arranged with chronological order
+    start_ai_solver :
+                    this start discovering game nodes starting with the middle of the board
+    
+    discover_node :
+                    discover the node and get the interesting nodes surrounded by it 
+                    by getting the return of 'discover_call_back' and add it to 'nodes_to_traverse'
+                    if the node is not in the 'mine_vault
+                    
+    start_discovering :
+                    loop through 'nodes_to_traverse' and send each node to 
+                    'start_weighting'
+                    
+    start_weighting :
+                    its role to find a node with weight equals to its neighbours this means its hundred percent a mine
+                    if his node is found send it to 'back_track_nodes'
+                                   
+    back_track_nodes :
+                    traverse the neighbours of the node to see if there is a save nodes 
+                    the safe nodes are added to 'high_priority_nodes'
+    
+    trigger_the_high_priority_nodes:
+                    it make sure that the node doesnt exist in the 'mine_vault' before passing it to 
+                    'discover_node'
+"""
+
 
 class AiController:
     def __init__(self, game_board: GameBoard, discover_call_back,
-                 discovered_nodes_highlight_call_back = None
-                 , ignored_nodes_highlight_call_back = None
-                 , add_flag_call_call_back = None):
+                 discovered_nodes_highlight_call_back=None
+                 , ignored_nodes_highlight_call_back=None
+                 , add_flag_call_call_back=None):
+        """
+
+        :param game_board: just the game board :)
+        :param discover_call_back: f(pos) -> [nodes] call back that returns the discovered nodes
+        :param discovered_nodes_highlight_call_back:
+        :param ignored_nodes_highlight_call_back:
+        :param add_flag_call_call_back:
+        """
         self.game_board = game_board
         self.discover_call_back = discover_call_back
         self.discovered_nodes_highlight_call_back = discovered_nodes_highlight_call_back
         self.ignored_nodes_highlight_call_back = ignored_nodes_highlight_call_back
         self.add_flag_call_call_back = add_flag_call_call_back
+
         # list holds the weighted nodes in order to traverse them later
         self.nodes_to_traverse = []
-        self.mine_vault = []
-        self.high_priority_nodes = []
-        self.discover_node((int(game_board.row / 2), int(game_board.col / 2)))
-        t = Thread(target=self.start_ui_solver, args=())
-        t.start()
 
-    def start_ui_solver(self):
+        # a list contain all the nodes that is hundred percent sure they are mines
+        self.mine_vault = []
+
+        # holds the nodes that are less likely to be mines
+        self.high_priority_nodes = []
+        self.ai_thread = Thread(target=self.start_ai_solver, args=())
+        self.ai_thread.start()
+
+    def start_ai_solver(self):
+        self.discover_node((int(self.game_board.row / 2), int(self.game_board.col / 2)))
         while self.game_board.game_state == 0:
             self.start_discovering()
             # time.sleep(0.5)
@@ -102,3 +143,6 @@ class AiController:
                 if node.node_data is None and node not in self.mine_vault:
                     self.discover_node(node.pos)
                     return
+
+    def wait_till_ai_finish(self):
+        self.ai_thread.join()
